9b22111e8b535d203a2c9fa6cbd94f700c2c906a
Amine-kun <aminehlab@gmail.com>
2022-09-06 18:15:48 +0200

    privat chat progress

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,6 +8,12 @@ const Contact = ({contact}) => {
 	const [spinner, setSpinner] = useState(false);
 	const [isOpened, setIsOpened] = useState(false);
 	const [showConvo, setShowConvo] = useState(false);
+	const [newFriend, setNewFriend] = useState(null);
+	const [initialMessage, setInitialMessage] = useState(null);
+
+	const userInfo = localStorage.getItem('Ecom-user') !== 'undefined'
+                 ? JSON.parse (localStorage.getItem('Ecom-user'))
+                  : localStorage.clear();
 
 	useEffect(() => {
 		if(isOpened)
@@ -15,6 +21,28 @@ const Contact = ({contact}) => {
 
 	}, [isOpened])
 
+	const submitContact = () => {
+
+		fetch(`http://localhost:3001/user/Contact`,{
+						              method:'post',
+						              headers:{'Content-Type': 'application/json'},
+						              body: JSON.stringify({
+						              	initialMessage:{
+						              		time:"20:00pm",
+						              		message:initialMessage,
+						              	},
+						              	newFriend:newFriend,
+						              	from:{
+						              		userId: userInfo._id,
+						              		userName: userInfo.name,
+						              		userImage:userInfo.userImage
+						              	}
+						              })
+		      						})
+
+					          	   .then((res)=>res.json())
+
+	}
 	
 	if (spinner) return <Spinner/>
 
@@ -23,18 +51,24 @@ const Contact = ({contact}) => {
 			<div className="users__container">
 
 				{!isOpened && 
-
-					<div className="user_convo pointer" onClick={()=>setIsOpened(true)}>
-						<img src={contact.from.userImage} className="sender-p" alt="userp"/>
-							<span className="name-msg">
-								<h4 className="text-set">{contact.from.userName}</h4>
-								<p className="text-set message">yoooooooo dude whatsupp, been ...</p>
-							</span>
-					</div>
+					<>
+						<div className="user_convo pointer" onClick={()=>setIsOpened(true)}>
+							<img src={userInfo._id === contact.from.userId ? contact.to.userImage : contact.from.userImage} className="sender-p" alt="userp"/>
+								<span className="name-msg">
+									<h4 className="text-set">{userInfo._id === contact.from.userId ? contact.to.userName : contact.from.userName}</h4>
+									<p className="text-set message">yoooooooo dude whatsupp, been ...</p>
+								</span>
+						</div>
+						<div>
+							<input type="text" placeholder="add a friend" onChange={(e)=>setNewFriend(e.target.value)}/>
+							<button onClick={()=> submitContact()}>Add</button>
+						</div>
+							<input type="text" placeholder="your message" onChange={(e)=>setInitialMessage(e.target.value)}/>
+					</>
 				}
 
 				{isOpened && 
-					<Convo showConvo={showConvo} contact={contact} setIsOpened={setIsOpened}/>
+					<Convo showConvo={showConvo} contact={contact} setIsOpened={setIsOpened} userInfo={userInfo}/>
 				}
 				
 			</div>
--- src/Components/Contact/Convo/Convo.jsx
+++ src/Components/Contact/Convo/Convo.jsx
@@ -2,41 +2,64 @@ import React, {useState, useEffect} from 'react';
 import './Convo.css';
 import { io }  from "socket.io-client";
 
-const Convo = ({showConvo, contact, setIsOpened}) => {
-
-	const userInfo = localStorage.getItem('Ecom-user') !== 'undefined'
-                 ? JSON.parse (localStorage.getItem('Ecom-user'))
-                  : localStorage.clear();
+const Convo = ({showConvo, contact, setIsOpened, userInfo}) => {
 
 	const [message, setMessage] = useState(null);
 	const [messages, setMessages] = useState(null);
-	let newlyConvo;
 	const [channel, setChannel] = useState({id:userInfo._id, name:userInfo.name});
 	const socket = io('http://localhost:3001');
 
 			useEffect(() => {
 
 				fetch(`http://localhost:3001/user/conversation/${contact._id}`,{
-		              method:'post',
+		              method:'get',
 		              headers:{'Content-Type': 'application/json'},
 		      	})
 		            .then((res)=>res.json())
 		             .then((data)=>{
-		 										newlyConvo = data.messages;
-		 										setMessages(newlyConvo);
+		 										setMessages(data.messages);
 		                     })
 					
 			}, [])
 
 			useEffect(() => {
 				 socket.on('receive_message', (data) => {
-				    setMessages(data);
-				})
-			}, [socket])
+				 		setMessages((prev) => [...prev, data])
+				});
+
+				 return () => {
+		        socket.disconnect();
+		      }
+
+			}, [])
 	
 
 				const sendMessage = () =>{
-					socket.emit("send_message", [{message:message, source:{userId:userInfo._id, userName:userInfo.name}, time:"20:00pm"}, {message:message, source:{userId:userInfo._id, userName:userInfo.name}, time:"20:00pm"}])
+					if (!message || message === ''){
+						console.log("enter a valid message")
+					} else {
+							socket.emit("send_message", {message:message, source:{userId:userInfo._id, userName:userInfo.name}});
+
+							fetch(`http://localhost:3001/user/conversation`,{
+						              method:'post',
+						              headers:{'Content-Type': 'application/json'},
+						              body: JSON.stringify({
+						              	convoId: contact._id,
+						              	time:"20:00pm",
+						              	message:message,
+						              	source:{
+						              		userId:userInfo._id,
+						              		userName:userInfo.name,
+						              		userImage:userInfo.userImage
+						              	}
+						              })
+		      						})
+
+					          	   .then((res)=>res.json())
+						             .then((data)=>{
+						 										console.log(data)
+						                     })
+								}
 			 }
  	
 	return (
